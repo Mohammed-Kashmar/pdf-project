687afae0ac08a90dcab5bcc0a127d2e44120c7cf
YaserKamel <yasermh61@gmail.com>
2024-03-27 14:29:39 +0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Homepage } from './pages/Homepage'
+import { LoginPage } from './pages/Admin/Auth/LoginPage'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
       </Routes>
     </BrowserRouter>
   )
